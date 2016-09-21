369fcfe83db45bed50d6379573b2a51b67990118
gnzlrm <gnzl.rm@gmail.com>
2016-09-21 09:22:23 -0300

    Refactored pretty much everything.

--- csv_wordcount.py
+++ csv_wordcount.py
@@ -6,209 +6,178 @@ Perform word-counts on columns of a CSV file.
 Created by:     Gonzalo Rolón Morinelli
 Last update by: Gonzalo Rolón Morinelli
 Creation date:  2016-09-12
-Last update:    2016-09-12
+Last update:    2016-09-21
 """
 
 import csv
-import os
-import sys
-import traceback
-
-
-# Globals.
-
-replaceable_chars = {"a": ["á", "ä", "à"],
-                     "e": ["é", "ë", "è"],
-                     "i": ["í", "ï", "ì"],
-                     "o": ["ó", "ö", "ò"],
-                     "u": ["ú", "ü", "ù"],
-                     "n": ["ñ"]}
-
-OUTPUT_FORMAT = "{}. {}:\t{}\n"
-
-# Core functions.
-
-
-def read_csv_and_count(t_file, t_cols, tidy_func = {}, replace_dict = {}):
-    """
-    Read a CSV file and perform word-count on target columns.
-
-    :param t_file:          A CSV file.
-    :param t_cols:          A list of indexes of target columns.
-    :param tidy_func:       A function to tidy strings.
-    :param replace_dict:    A dict with replacements as keys and a list of
-                            replaceables for that replacement.
-    :return:                A dict containing the results.
-    """
-    reader = csv.reader(t_file)
-    reader.__next__()
-    result = {t_col: {} for t_col in t_cols}
-    for row in reader:
-        for t_col in t_cols:
-            add_answer_to_result(result, row, t_col, tidy_func,
-                                 replace_dict)
-    return result
-
-
-def add_answer_to_result(result, row, t_col, tidy_func, replace_dict):
-    """
-    Add answers to the result dict.
-
-    :param result:          A result dict used to store count of answers for
-                            target columns.
-    :param row:             The current row.
-    :param t_col:           The column being analyzed.
-    :param tidy_func:       A function to tidy strings.
-    :param replace_dict:    A dict with replacements as keys and a list of
-                            replaceables for that replacement.
-    :return:                None. Modifies result.
-    """
-    for answer in row[t_col].split(","):
-        answer = tidy_func(answer, replace_dict)
-        result[t_col][answer] = result[t_col].setdefault(answer, 0) + 1
+import re
+from unidecode import unidecode
 
 
 # Tidy functions.
-# TODO: Come up with more tidy functions!
 
-def tidy_string_def(some_string, replace_dict):
+def tidy_strings(string_list):
     """
-    Tidy a string by removing trailing and starting whitespace, enforce
-    lowercase and replace chars as defined in REPLACEABLE_CHARS.
+    Tidy a list of strings by removing trailing and starting whitespace, enforce
+    lowercase and replace characters.
 
-    :param some_string:     A string to be tidied
-    :param replace_dict:    A dict with replacements as keys and a list of
-                            replaceables for that replacement.
+    :param string_list:     A string to be tidied
+    :param repl_dict:       A dict with replacements as keys and a list of
+                            replaceables for that replacement as values.
     :return:                A tidied string
     """
-    some_string = some_string.lower()
-    for replacement in replace_dict:
-        for replaceable in replace_dict[replacement]:
-            some_string = some_string.replace(replaceable, replacement)
-    return some_string.strip()
+    res = []
+    for string in string_list:
+        string = unidecode(string.lower())
+        res.append(string.strip())
+    return res
+
+
+# Core functions.
+
+def csv_wordcount(file, cols, del_exp, tidy_func=tidy_strings):
+    """
+    Read a CSV file and perform word-count on target columns.
+
+    :param file:            A CSV file.
+    :param cols:            A list of indexes of target columns.
+    :param tidy_func:       A function to tidy a list of strings.
+    :param del_exp:         A string with delimiter characters to split
+    answers.
+    :return:                A dict containing the results.
+    """
+
+    def add_answers():
+        """Add the answers to the result."""
+        print(del_exp)
+        for answ in set(tidy_func(
+                        filter(None, re.split("[\W]+", row[target[0]])))):
+            result[target][answ] = result[target].setdefault(answ, 0) + 1
+
+    print(del_exp)
+    reader = csv.reader(file)
+    headers = reader.__next__()
+    headers = [headers[col] for col in cols]
+    result = {(col, header): {} for col, header in zip(cols, headers)}
+    for row in reader:
+        for target in result:
+            add_answers()
+    return result
 
 
 # Create result output.
 
-def sort_and_save_dict(result, out_path):
+def sort_and_write_result(result, file, out_format, sort_func=lambda x: x[1],
+                          desc=True):
     """
     Sorts and saves to a txt file a dict of dicts with sortable values.
 
     :param result:      A result dict used to store count of answers for
                         target columns.
-    :param out_path:    The path to the output file.
+    :param file:    The output file.
+    :param out_format:  A string modeling the output format for result rows
+                        that contains three placeholders for idx, word and
+                        count.
+    :param sort_func:   A function to perform sorting. Defaults to sorting by
+                        frequency.
+    :param desc         Boolean for sorting in descending order.
     :return:            None
     """
-    buffer = ""
+    buffer = "\n" + "-" * 80 + "\n\n"
     for field in result:
         f_dict = result[field]
-        buffer += "Results for: {}\n".format(field)
-        buffer += "\n"
-        buffer += "Alphabetical sorting:\n"
-        buffer += "\n"
-        for idx, tuple_key in enumerate(sorted(f_dict.items())):
-            buffer += OUTPUT_FORMAT.format(idx, tuple_key[0], tuple_key[1])
-        buffer += "\n"
-        # Ranked
-        buffer += "Ranked sorting:\n"
-        buffer += "\n"
-        sort_val = lambda x: x[1]
-        for idx, tuple_key in enumerate(sorted(f_dict.items(), key=sort_val,
-                                               reverse=True)):
-            buffer += OUTPUT_FORMAT.format(idx, tuple_key[0], tuple_key[1])
-        buffer += "\n"
-        buffer += "-" * 80
-        buffer += "\n"
-    with open(out_path, "w") as out_file:
-        out_file.write(str(buffer))
+        buffer += "Results for: {}\n\n".format(field[1])
+        for idx, tuple_key in enumerate(sorted(f_dict.items(), key=sort_func,
+                                               reverse=desc)):
+            buffer += out_format.format(idx + 1, tuple_key[1], tuple_key[0])
+        buffer += "\n" + "-" * 80 + "\n\n"
+    file.write(buffer)
 
 
 # CLI-mode.
 
 if __name__ == "__main__":
 
+    import argparse
+    import os
+    import sys
+    import traceback
+
+    IN_FILE = 'file'
+    OUT_FILE = 'out'
+    COLS = 'cols'
+
+    DES_PARSER = "Count words on the columns of a CSV file."
+    DES_COLS = "list of target columns indexes"
+    DES_OUT = "Path to output file (created if it doesn't exists)"
+
+    OUT_FORMAT = "{}. {} times: {}\n"
+
     # Core functions of CLI-mode
 
-    def input_target_columns(t_path):
+    def close_file_and_exit():
+        """Pretty self explanatory."""
+        args[IN_FILE].close()
+        sys.exit()
+
+
+    def input_target_columns(args):
         """Interactive input mode for target columns."""
-        print("[+} No target columns provided. Please, choose from the "
+        print("No target columns provided. Please, choose from the "
               "following list:")
-        with open(t_path, encoding="utf-8") as t_file:
-            reader = csv.reader(t_file)
-            headers = reader.__next__()
-            for (idx, header) in enumerate(headers):
-                print(str(idx) + ".", header)
-        print()
-        t_cols = set()
+        reader = csv.reader(args[IN_FILE])
+        headers = reader.__next__()
+
+        for (idx, header) in enumerate(headers):
+            print("\t" + str(1 + idx) + ".", header)
         cols_input = input(
-                "Enter comma-separated list of target columns (empty = all "
-                "columns): ")
+            "Enter a list of target columns indexes (empty = all "
+            "columns): ")
         if cols_input:
             try:
-                t_cols = {int(t_col) for t_col in cols_input.split(",")}
+                cols = [int(t_col) - 1 for t_col in cols_input.split(" ")]
+                for col in cols:
+                    if col not in range(len(headers)):
+                        print("error: Invalid column index provided: ", col)
+                        close_file_and_exit()
+                args[COLS] = cols
             except ValueError as err:
-                error_parsing_t_cols(err)
-        return t_cols
+                print("error: Columns indexes couldn't be parsed: ", err)
+                close_file_and_exit()
+        else:
+            args[COLS] = range(len(headers))
 
+    # Args registration and validation.
 
-    # Helpers for CLI-mode
+    parser = argparse.ArgumentParser(description=DES_PARSER)
+    parser.add_argument("--cols", nargs="*", dest=COLS,
+                        type=int, metavar="N", help=DES_COLS)
+    parser.add_argument(IN_FILE, metavar="path/to/file.csv",
+                        type=argparse.FileType('r', encoding="utf-8"))
+    parser.add_argument("--out", metavar="path/to/output", help=DES_OUT,
+                        type=argparse.FileType('w+', encoding="utf-8"),
+                        default=sys.stdout, dest=OUT_FILE)
+    args = vars(parser.parse_args())
 
-    def print_usage():
-        """Print the usage of the tool."""
-        print()
-        print("usage:   wordcount_csv.py path/to/file.csv [0-indexed "
-              "comma-separated list of target column indexes] ["
-              "comma-separated list of case-insensitive strings to be omitted "
-              "from answers]")
+    if os.path.splitext(args[IN_FILE].name)[-1] != ".csv":
+        print("error: Input file isn't a valid CSV file.")
+        parser.print_help()
+        close_file_and_exit()
+    if not args[COLS]:
+        input_target_columns(args)
 
+    # Call to core functions.
 
-    def error_parsing_t_cols(err):
-        """Print the error for parsing target columns along with usage."""
-        print("[-] Target columns couldn't be parsed: ", err)
-        print_usage()
-        sys.exit()
-
-
-    # The actual script.
-    # TODO: Add argv select tidy function to be applied.
-
-    # Unpack target path from argv.
+    wordcount_dict = {}
     try:
-        t_path = sys.argv[1]
-        t_ext = os.path.splitext(t_path)[-1].lower()
-        if not os.path.isfile(t_path) and t_ext != ".csv":
-            print("[-] Input path isn't a valid CSV file.")
-            print_usage()
-            sys.exit()
-    except IndexError:
-        print_usage()
-        sys.exit()
-
-    # Unpack target columns from argv or enter interactive mode to select.
-    try:
-        t_cols = [int(t_col) for t_col in sys.argv[2].split(",")]
-    except IndexError:
-        t_cols = input_target_columns(t_path)
-    except ValueError as err:
-        error_parsing_t_cols(err)
-
-    # Unpack omit strings from argv.
-    try:
-        replaceable_chars[""] = [string.lower() for string in sys.argv[3].split(
-                                ",")]
-    except IndexError:
-        pass
-
-    # Call the core functions.
-    result = {}
-    try:
-        with open(t_path, encoding="UTF-8") as t_file:
-            result = read_csv_and_count(t_file, t_cols, tidy_string_def,
-                                        replaceable_chars)
+        wordcount_dict = csv_wordcount(args[IN_FILE], args[COLS], tidy_strings)
     except Exception as err:
-        print("[-] There was an error while analyzing:", err)
-        traceback.print_exc(flush=True)
-    sort_and_save_dict(result, "wordcount_result.txt")
+        print("There was an error while analyzing:", err)
+        traceback.print_exc()
+        close_file_and_exit()
+    args[IN_FILE].close()
+    sort_and_write_result(wordcount_dict, args[OUT_FILE], OUT_FORMAT)
+    args[OUT_FILE].close()
 
 
 
